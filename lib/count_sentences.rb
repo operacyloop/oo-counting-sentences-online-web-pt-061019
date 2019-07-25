06578bdae47bb5d6ab56d8a5e34a262a215b038c
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-07-25 23:42:56 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
-
+    
   def count_sentences
-
+    sentence_array = self.split(/[.?!]/)
+    sentence_array.delete_if{|sentence| sentence.empty?}
+    sentence_array.length
   end
+    
 end
